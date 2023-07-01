e3410ce377ccbb26ba74dc4934c35145370c12e8
mohsen moradi <mohsenun7@gmail.com>
2023-07-01 09:10:14 -0400

    add file size validator

--- src/ipfs/ipfs.module.ts
+++ src/ipfs/ipfs.module.ts
@@ -4,9 +4,13 @@ import { Ipfs } from './entities/ipfs.entity';
 import { IpfsController } from './ipfs.controller';
 import { IpfsService } from './ipfs.service';
 import { HeliaService } from './helia.service';
+import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Ipfs])],
+  imports: [
+    TypeOrmModule.forFeature([Ipfs]),
+    MulterModule.register({ limits: { fileSize: 100 * 1024 * 1024 } }),
+  ],
   controllers: [IpfsController],
   providers: [IpfsService, HeliaService],
 })
